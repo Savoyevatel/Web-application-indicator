506fa8a50bb1d9830f1d4267a27aff286906ede8
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2023-11-28 04:03:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from parsing_moveset import *
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -23,7 +25,16 @@ def greeting(name):
         return render_template('greeting.html', var1=var1, var2=var2, var3=var3)
 
     else:
-        return redirect(url_for('index'))    
+        return redirect(url_for('index')) 
    
+@socketio.on('attack')
+def handle_attack(data):
+    move_name = data['move_name']
+    damage = data['damage']
+    player_id = request.sid
+    print(f"Player {player_id} used attack: {move_name} with {damage} damage")
+    #print(f"Used attack: {move_name} with {damage} damage")
+    socketio.emit('update_player_info', {'player_id': player_id, 'health': 50}) 
+
 if __name__ == '__main__':
     app.run(debug=True)
